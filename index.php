2ad653e13d188d24fbbd0747efa84c67a2ab5043
5eafearfil <106165077+5eafearfil@users.noreply.github.com>
2022-05-28 18:53:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,3 +7,8 @@ header('X-Frame-Options: DENY');
 header("X-XSS-Protection: 1; mode=block");
 header("X-XSS-Protection: 1; mode=block");
 ?> 
+<?
+include 'config.php';
+?>
+<!-- https://dev.vk.com/api/access-token/authcode-flow-user -->
+<a href="https://oauth.vk.com/authorize?client_id=<?=ID?>&display=page&redirect_uri=<?=URL?>&scope=photos&response_type=code&v=5.131" target="_blank">VK AUTHORIZE</a>
